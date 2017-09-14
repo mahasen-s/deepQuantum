0634d3cabb33f8f91931120304e2664d96d7fd79
Mahasen <mahasen.sooriyabandara@anu.edu.au>
2017-09-14 14:08:27 +1000

    Clean repo for refactor

--- exact/pythonLoadHDF5.py
+++
@@ -1,9 +0,0 @@
-def loadMatlabHDF5(fname,field):
-    import h5py
-    import numpy as np
-
-    with h5py.File(fname) as f:
-        N   = f[field][0].shape[0];
-        data = [[f[element[ind]][0] for element in f[field]] for ind in np.arange(N)]
-
-    return data
--- lib/config.py
+++
@@ -1,3 +0,0 @@
-# Set default dtype
-import tensorflow as tf
-DTYPE    = tf.complex128

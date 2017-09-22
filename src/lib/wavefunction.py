a2cf6753af04a3a839a0212fe56bfe4fc383b7bc
Mahasen <mahasen.sooriyabandara@anu.edu.au>
2017-09-22 12:00:34 +1000

    update

--- src/lib/wavefunction.py
+++ src/lib/wavefunction.py
@@ -9,7 +9,7 @@ import tensorflow as tf
 
 class wavefunction():
 
-    def __init__(self,sess=tf.get_default_session(),num_sites,num_hidden=None, num_layers=1, act_fun="sigmoid"):
+    def __init__(self,sess=tf.get_default_session(),num_sites,num_hidden=None, num_layers=1, act_fun="sigmoid", var_dtype=tf.float64):
         # Process inputs
         if num_hidden is None:
             num_hidden = num_sites
@@ -20,11 +20,44 @@ class wavefunction():
         self.num_hidden = num_hidden
         self.act_fun    = act_fun
 
-
         # Create funcs to initialise weights/biases
-        tf_weight_gen   = lambda x: tf.Variable(tf.random_normal(x, dtype=conf.DTYPE))
-        tf_bias_gen     = lambda x: tf.Variable(tf.random_normal(x, dtype=conf.DTYPE))
+        tf_weight_gen   = lambda s,x: tf.get_variable(s,x, dtype=conf.DTYPE, initializer=tf.random_normal_initializer)
+        tf_bias_gen     = lambda s,x: tf.get_variable(s,x, dtype=conf.DTYPE, initializer=tf.random_normal_initializer)
 
+        # Create list of weights, biases
+        self.biases     = []
+        self.weights    = []
+
+        with tf.variable_scope("wavefunction"):
+            for ind in range(num_layers):
+                str_biases  = "bias_layer_%d" %(ind)
+                str_weights = "weights_layer_%d" %(ind)
+
+                self.biases.append(tf_bias_gen(str_biases,self.hidden_num))
+
+                if ind==0:
+                    self.weights.append(tf_weight_gen(str_weights,[self.hidden_num,self.input_num]))
+                else:
+                    self.weights.append(tf_weight_gen(str_weights,[self.hidden_num,self.hidden_num]))
+
+    def buildOp(self,input_states):
+        # generate einsum string
+        input_dims  = len(input_states.get_shape());
+        alphabet    = 'abcdefghklmnopqrstuvwxyz';
+        einsum_string   = 'ij,%sj->%si' %(alphabet[0:input_dims-1],alphabet[0:input_dims-1])
+
+
+        # need an operator which will compute the wavefunction on the innermost dimension
+        for inf in range(self.num_layers):
+            if ind==0:
+                curr_net    = self.act_fun(tf.add(self.biases[ind],tf.einsum(einsum_string,self.weights[ind],input_states)))
+            else:
+                curr_net    = self.act_fun(tf.add(self.biases[ind],tf.einsum(einsum_string,self.weights[ind],curr_net)))
+
+        # shoudl there be an activation function on this?
+        psi = tf.add(self.biases[-1],tf.einsum(einsum_string[0:-1],self.weights[-1],curr_net))
+
+        return psi
 
 
 

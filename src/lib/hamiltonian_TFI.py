5ba7aae74c14077bd7f3c919681ca25b60561946
Mahasen <mahasen.sooriyabandara@anu.edu.au>
2017-09-29 11:01:41 +1000

    Energy seems to work, need to test now

--- src/lib/hamiltonian_TFI.py
+++ src/lib/hamiltonian_TFI.py
@@ -13,7 +13,7 @@ import utils as utils
 class hamiltonian_tfi():
     # Produces graphs which compute the local energy and summary statistics for a given input and 
     # wavefunction
-    def __init__(self,h_drive=1,h_inter=1,h_detune=0,dtype=tf.float32):
+    def __init__(self,h_drive=1,h_inter=1,h_detune=0,dtype=tf.float64):
         self.h_drive    = h_drive
         self.h_inter    = h_inter
         self.h_detune   = h_detune
@@ -31,35 +31,36 @@ class hamiltonian_tfi():
         num_sites       = input_states.get_shape().as_list()[-1]
         input_dims      = len(input_states.get_shape())
 
-        sigz            = utils.tf_roll(input_states,1,axis=input_dims-1)
+        sigz            = tf.reduce_sum(tf.multiply(input_states,utils.tf_roll(input_states,1,axis=-1)),axis=-1,keep_dims=True)
 
         einsum_string   = '%sk,ak->%sak' %(self.alphabet[0:input_dims-1],self.alphabet[0:input_dims-1])
         flip_mat        = tf.subtract(tf.ones(num_sites,dtype=dtype),tf.scalar_mul(2.0,tf.eye(num_sites,dtype=dtype)))
         sigx            = tf.einsum(einsum_string,input_states,flip_mat)
 
         # Get energy from sigz, sigx
-        E_sigz          = tf.scalar_mul(self.h_inter,tf.multiply(self.psi,self.sigz)) # will fail because dimensions of psi, sigz are wrong
+        psi             = wavefunction.build_wf(input_states)
+        E_sigz          = tf.scalar_mul(self.h_inter,tf.multiply(psi,sigz)) # will fail because dimensions of psi, sigz are wrong
         E_sigx          = tf.scalar_mul(self.h_drive,tf.reduce_sum(wavefunction.build_wf(sigx),axis=-1))
 
-
-
-        psi     = wavefunction.build_wf(input_states)
-        E_sigz  = tf.scalar_mul(self.h_inter,tf.multiply(psi,sigz))
+        # Output energy
 
 
 def test_hamiltonian_TFI():
     import wavefunction as wavefunction
-    W = wavefunction.wavefunction(num_sites=5,num_hidden=7,num_layers=3)
+    import numpy.random as nr
+    wf = wavefunction.wavefunction(num_sites=5,num_hidden=7,num_layers=3)
     sess = tf.Session()
     sess.run(tf.global_variables_initializer())
-    
 
-    input_1D    = tf.constant(np.ones([5]))
-    input_2D    = tf.constant(np.ones([4,5]))
-    input_3D    = tf.constant(np.ones([3,4,5]))
+    input_1D    = tf.constant(2*nr.randint(0,high=2,size=[5]).astype(float)-1.0)
+    input_2D    = tf.constant(2*nr.randint(0,high=2,size=[4,5]).astype(float)-1.0)
+    input_3D    = tf.constant(2*nr.randint(0,high=2,size=[3,4,5]).astype(float)-1.0)
 
     op_1D       = wf.build_wf(input_1D)
     op_2D       = wf.build_wf(input_2D)
     op_3D       = wf.build_wf(input_3D)
 
-    Î
+    import pdb; pdb.set_trace()
+
+def test_hamiltonian_TFI_energy():
+    return 1

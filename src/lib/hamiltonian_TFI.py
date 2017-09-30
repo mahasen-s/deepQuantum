d111912873dddb00fcc9638f237d12932f621100
Mahasen <mahasen.sooriyabandara@anu.edu.au>
2017-09-30 14:06:32 +1000

    nothing much really

--- src/lib/hamiltonian_TFI.py
+++ src/lib/hamiltonian_TFI.py
@@ -17,6 +17,7 @@ class hamiltonian_tfi():
         self.h_drive    = h_drive
         self.h_inter    = h_inter
         self.h_detune   = h_detune
+        self.dtype      = dtype
         self.alphabet   = 'abcdefghklmnopqrstuvwxyz';
 
     def build_energy_local(self,input_states=None,wavefunction=None):
@@ -24,28 +25,32 @@ class hamiltonian_tfi():
             raise ValueError('input_states must be provided')
         if wavefunction is None:
             raise ValueError('wavefunction must be provided')
+        input_dims      = len(input_states.get_shape())
         if input_dims>24:
             raise ValueError('Number of dimensions of input_states too large (>24)')
 
         # Calculate sigz, sigx
         num_sites       = input_states.get_shape().as_list()[-1]
-        input_dims      = len(input_states.get_shape())
 
         sigz            = tf.reduce_sum(tf.multiply(input_states,utils.tf_roll(input_states,1,axis=-1)),axis=-1,keep_dims=True)
 
-        einsum_string   = '%sk,ak->%sak' %(self.alphabet[0:input_dims-1],self.alphabet[0:input_dims-1])
-        flip_mat        = tf.subtract(tf.ones(num_sites,dtype=dtype),tf.scalar_mul(2.0,tf.eye(num_sites,dtype=dtype)))
+        einsum_string   = '%si,ji->%sij' %(self.alphabet[0:input_dims-1],self.alphabet[0:input_dims-1])
+        flip_mat        = tf.subtract(tf.ones(num_sites,dtype=self.dtype),tf.scalar_mul(2.0,tf.eye(num_sites,dtype=self.dtype)))
         sigx            = tf.einsum(einsum_string,input_states,flip_mat)
 
         # Get energy from sigz, sigx
         psi             = wavefunction.build_wf(input_states)
         E_sigz          = tf.scalar_mul(self.h_inter,tf.multiply(psi,sigz)) # will fail because dimensions of psi, sigz are wrong
-        E_sigx          = tf.scalar_mul(self.h_drive,tf.reduce_sum(wavefunction.build_wf(sigx),axis=-1))
+        E_sigx          = tf.scalar_mul(self.h_drive,tf.reduce_sum(wavefunction.build_wf(sigx),axis=-2))
 
         # Output energy
+        E_proj_unnorm   = tf.add(E_sigz,E_sigx);
+        E_local         = tf.divide(E_proj_unnorm,psi)
 
+        return E_local
 
 def test_hamiltonian_TFI():
+    # check that output is dimensionally correct
     import wavefunction as wavefunction
     import numpy.random as nr
     wf = wavefunction.wavefunction(num_sites=5,num_hidden=7,num_layers=3)
@@ -60,7 +65,35 @@ def test_hamiltonian_TFI():
     op_2D       = wf.build_wf(input_2D)
     op_3D       = wf.build_wf(input_3D)
 
-    import pdb; pdb.set_trace()
+    H           = hamiltonian_tfi()
+    H_1D        = H.build_energy_local(input_states=input_1D,wavefunction=wf)
+    H_2D        = H.build_energy_local(input_states=input_2D,wavefunction=wf)
+    H_3D        = H.build_energy_local(input_states=input_3D,wavefunction=wf)
 
-def test_hamiltonian_TFI_energy():
+    assert sess.run(H_1D).shape==(2,)
+    assert sess.run(H_2D).shape==(4,2)
+    assert sess.run(H_3D).shape==(3,4,2)
+
+
+
+def ntest_hamiltonian_TFI_energy():
+    import wavefunction as wavefunction
+    import numpy.random as nr
+
+    # Make wavefunction
+    wf = wavefunction.wavefunction(num_sites=5,num_hidden=7,num_layers=3)
+    sess = tf.Session()
+
+    # Gen all states
+    [states_sites,states_hilbert]   = utils.genAllStates(5)
+
+    # Load exact energy, hDrive=1, hInter=1, hDetune=0
+    exactFile = "../exact/results/exact_TFI_hDrive=1p0_hInter=1p0_hDetune=0p0.mat"
+
+    # Make wavefunction
+
+    # Make Hamiltonian
+    H   = hamiltonian_tfi()
+    E_local     = H.build_energy_local(states_sites)
+
     return 1
--- src/lib/utils.py
+++ src/lib/utils.py
@@ -4,3 +4,45 @@ import numpy as np
 def tf_roll(x,n,axis=0):
     # Equivalent to numpy's roll
     return tf.gather(x,np.roll(np.arange(x.get_shape().as_list()[axis]),n),axis=axis)
+
+def genAllStates(N,dtype=float):
+    import numpy as np
+
+    binarise = lambda x: np.array(list(format(x,'0'+str(N)+'b'))).astype(dtype)
+
+    # Generate states as 0,1 site-indexed
+    states  = np.zeros([pow(2,N),N],dtype=dtype);
+    for i in range(pow(2,N)):
+        states[i,:]     = binarise(i)
+
+    # Generate states as element of Hilbert space
+    up      = np.array([0,1])
+    down    = np.array([1,0])
+    states_hilbert   = np.zeros([pow(2,N),pow(2,N)],dtype=dtype)
+    for i in range(pow(2,N)):
+        newState = 1
+        for j in range(N):
+            if states[i,j] == 1:
+                newState    = np.kron(newState,up)
+            else:
+                newState    = np.kron(newState,down)
+        states_hilbert[i,:] = newState
+
+    # Generate states as [-1,1] site indexed
+    states_sites = 2*states-1
+    return states_sites, states_hilbert
+
+class Bunch(dict):
+    def __init__(self, *args, **kwds):
+        super(Bunch, self).__init__(*args, **kwds)
+        self.__dict__ = self
+
+def loadMatlabHDF5(fname,field):
+    import h5py
+    import numpy as np
+
+    with h5py.File(fname) as f:
+        N   = f[field][0].shape[0];
+        data = [[np.array(f[element[ind]][0]) for element in f[field]] for ind in np.arange(N)]
+
+    return data
